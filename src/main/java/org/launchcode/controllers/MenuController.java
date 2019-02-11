c056f1b88a9d09fd257181602f208931caf3595f
David Wood <dw@d4v1d.us>
2019-02-11 15:36:26 -0600

    part 3

---
+++ src/main/java/org/launchcode/controllers/MenuController.java
@@ -0,0 +1,30 @@
+package org.launchcode.controllers;
+
+import org.launchcode.models.data.CheeseDao;
+import org.launchcode.models.data.MenuDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping(value = "menu")
+public class MenuController {
+
+    @Autowired
+    private CheeseDao cheeseDao;
+
+    @Autowired
+    private MenuDao menuDao;
+
+
+    @RequestMapping(value = "")
+    public String index(Model model) {
+
+        model.addAttribute("menus", menuDao.findAll());
+        model.addAttribute("title", "All Menus");
+
+        return "menu/index";
+    }
+}
--- src/main/java/org/launchcode/models/Cheese.java
+++ src/main/java/org/launchcode/models/Cheese.java
@@ -1,11 +1,9 @@
 package org.launchcode.models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.List;
 
 /**
  * Created by LaunchCode
@@ -25,10 +23,11 @@ public class Cheese {
     @Size(min=1, message = "Description must not be empty")
     private String description;
 
-//    private CheeseType type;
-
     @ManyToOne
     private Category category;
+
+    @ManyToMany(mappedBy = "cheeses")
+    private List<Menu> menus;
 
     public Cheese(String name, String description) {
         this.name = name;
